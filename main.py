806dc3090316d84287fa2c1ca72317cb769351a3
YASH SANJAY PATIL <patilyash02121@gmail.com>
2025-12-05 23:18:42 +0530

    Updated the frontend url

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = FastAPI(title="Portfolio AI Backend")
 origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://portfolio-yash-patil.vercel.app/",
+    "https://portfolio-yash-patil.vercel.app",
 ]
 
 app.add_middleware(
