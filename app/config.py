9af30290d2e3583ce1f52b5fa37b01e18a9e20a6
YASH SANJAY PATIL <86646297+yashpatil02121@users.noreply.github.com>
2026-09-06 21:28:50 +0530

    model update

--- app/config.py
+++ app/config.py
@@ -4,4 +4,4 @@ from dotenv import load_dotenv
 load_dotenv()
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-MODEL_NAME = os.getenv("MODEL_NAME", "gemini-flash-latest")
+MODEL_NAME = os.getenv("MODEL_NAME", "gemini-3.5-flash-lite")
